430bd026c54e9a15f48052a20d537a08039a57cc
SORB <audinay.bryandave@gmail.com>
2026-05-02 11:09:11 -0400

    Update script URL for SORONICE HUB

--- LICENSE/Bibliothèque de SCRIPTS/Ouvrage/SORONICE HUB.lua
+++ LICENSE/Bibliothèque de SCRIPTS/Ouvrage/SORONICE HUB.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet('https://resonant-exposable-coil.ngrok-free.dev/'))()
+loadstring(game:HttpGet('https://customer-assets.emergentagent.com/job_df6bf601-f394-4757-a369-9a50404c2a7a/artifacts/752ese05_SORONICE%20HUB%20------.html'))()
